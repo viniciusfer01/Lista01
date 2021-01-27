e880d1044ee417e6afce51360f75295ac29ec4a0
Vinícius Fernandes <business.fernandes22@gmail.com>
2021-01-27 19:02:31 -0300

    Update Q03.c

--- Q03/Q03.c
+++ Q03/Q03.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
-void main() {
-
-    int i = 10, j = 12;
-    int *p = &i, *q;
-
-    printf("%p", q);
-
-    //p = i; - Expressão ilegal, pois p é um ponteiro e não pode receber o valor da variável inteira i;
+    //p = i; - Expressão ilegal, pois p é um ponteiro e não pode receber o valor da variável inteira i (apesar do programa funcionar);
     //q = &j; - Expressão legal;
     //p = &*&i; - Expressão legal;
     //i = (*&)j; - Expressão ilegal, pois os parênteses geram erro no programa;
@@ -15,5 +8,12 @@ void main() {
     //i = *&*&j; - Expressão legal;
     //q = *p; - Expressão ilegal, pois precisa de casting para funcionar;
     //i = (*p)++ + *q; - Expressão ilegal;
+
+void main() {
+
+    int i = 10, j = 12;
+    int *p = &i, *q;
+
+    printf("%p", q);
 
 }
